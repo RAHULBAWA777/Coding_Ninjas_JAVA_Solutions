1c58535495403d91794f0c0cbde809659e2f6aca
RAHULBAWA <rahulbawa514@gmail.com>
2022-11-06 16:57:00 +0530

    adding

--- lecture_5_Patterns1/CharacterPattern.java
+++ lecture_5_Patterns1/CharacterPattern.java
@@ -6,10 +6,6 @@ public class CharacterPattern {
 
 public static void main(String[] args) {
 		
-		/* Your class should be named Solution.
-	 	* Read input as specified in the question.
-	 	* Print output as specified in the question.
-		*/
         Scanner sc= new Scanner(System.in);
         int N = sc.nextInt();
         int m;
---
+++ lecture_5_Patterns1/InterestingAlphabets.java
@@ -0,0 +1,24 @@
+package lecture_5_Patterns1;
+
+import java.util.Scanner;
+
+public class InterestingAlphabets {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int m;
+		char c = (char) (65 + N);
+		for (int i = 1; i <= N; i++) {
+			c = (char) (c - i);
+			for (int j = 0; j < i; j++) {
+				System.out.print(c);
+				c++;
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
