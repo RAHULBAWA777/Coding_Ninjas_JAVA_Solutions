672f0db91479babeacfe7817c9e2ba65ae9ef69f
RAHULBAWA <rahulbawa514@gmail.com>
2022-11-06 16:18:26 +0530

    Update TriangleStarPattern.java

--- lecture_5_Patterns1/TriangleStarPattern.java
+++ lecture_5_Patterns1/TriangleStarPattern.java
@@ -1,10 +1,18 @@
 package lecture_5_Patterns1;
-
+import java.util.Scanner;
 public class TriangleStarPattern {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(n);
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
