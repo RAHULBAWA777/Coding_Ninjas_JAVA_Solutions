c4c1ef19c032c781a9a0e6117a42cb33b8f464d1
RAHULBAWA <rahulbawa514@gmail.com>
2022-11-09 12:22:50 +0530

    updates

--- lecture_12_TwoDimentionalArray/A2_PrintLikeAWave.java
+++ lecture_12_TwoDimentionalArray/A2_PrintLikeAWave.java
@@ -1,10 +1,31 @@
 package lecture_12_TwoDimentionalArray;
 
+import java.util.*;
+
 public class A2_PrintLikeAWave {
 
-	public static void main(String[] args) {
-		
+	public static int[] main(String[] args) {
+		int arr[][] = null;
+		int rows = 0;
+		int cols = 0;
+		int[] ans = new int[rows * cols];
+		int index = 0;
 
+		for (int j = 0; j < cols; j++) {
+			if (j % 2 == 0) {
+				// DOWN
+				for (int i = 0; i < rows; i++) {
+					ans[index++] = arr[i][j];
+				}
+			}
+			else {
+//				UP
+				for(int i=rows-1;i>=0;i--) {
+					ans[index++]=arr[i][j];
+				}
+			}
+		}
+		return ans;
 	}
 
 }
